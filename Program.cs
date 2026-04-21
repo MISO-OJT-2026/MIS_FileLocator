fc3e55079b6f3daa2804fb2fa58919a9d86e92b0
Bea <b.rigor00282@student.tsu.edu.ph>
2026-04-21 21:01:26 +0800

    pls pls pls don't prove i'm right

--- Program.cs
+++ Program.cs
@@ -40,7 +40,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString), ServiceLifetime.Scoped);
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
